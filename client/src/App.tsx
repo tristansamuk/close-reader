00eb94e21380116a1e95c6fb13ed836857c728aa
Tristan Samuk <tsamuk@gmail.com>
2024-01-27 12:01:17 -0500

    Add Tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
