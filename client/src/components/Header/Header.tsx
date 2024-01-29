df4e5df4991b2643ed6e2cb81e2d5c43c85177f8
Tristan Samuk <tsamuk@gmail.com>
2024-01-29 15:47:21 -0500

    Create animated pop-out menu for mobile and tablet view

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,32 +1,49 @@
 import "./Header.scss";
 import Hamburger from "hamburger-react";
 import magnifyingGlass from "../../assets/images/icons/magnifying-glass.svg";
+import { useState } from "react";
 
 const Header: React.FC = () => {
-  // Click handler for hamburger to display menu
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <header className="header">
-      <div className="header__box-left">
-        <Hamburger size={24} />
-        <h1 className="header__logo">CloseReader</h1>
-      </div>
-      <div className="header__box-right">
-        <img
-          src={magnifyingGlass}
-          alt="magnifying glass"
-          className="header__mag-icon"
-        />
-        {/* Search Bar */}
-        <label>
-          <input
-            name="search"
-            className="header__search"
-            type="text"
-            placeholder="Search..."
+    <>
+      <header className="header">
+        <div className="header__box-left">
+          <Hamburger toggled={isOpen} toggle={setOpen} size={24} />
+          <h1 className="header__logo">CloseReader</h1>
+        </div>
+        <div className="header__box-right">
+          <img
+            src={magnifyingGlass}
+            alt="magnifying glass"
+            className="header__mag-icon"
           />
-        </label>
-      </div>
-    </header>
+          {/* Search Bar */}
+          <label>
+            <input
+              name="search"
+              className="header__search"
+              type="text"
+              placeholder="Search..."
+            />
+          </label>
+        </div>
+      </header>
+      {/* Popout Menu */}
+      {isOpen && (
+        <>
+          <div className="header__popout-menu">
+            <ul className="header__nav-list">
+              <li className="header__nav-item">Authors</li>
+              <li className="header__nav-item--2">Poems</li>
+              <li className="header__nav-item--3">Collections</li>
+            </ul>
+          </div>
+          <div className="header__overlay"></div>
+        </>
+      )}
+    </>
   );
 };
 
