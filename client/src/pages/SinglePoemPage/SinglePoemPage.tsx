adbd62be2d28657ad810580301a09b9f8d6a7387
Tristan Samuk <tsamuk@gmail.com>
2024-01-30 23:27:25 -0500

    Render poem on page, some typescript errors continue to scream at me even though the site is working

--- client/src/pages/SinglePoemPage/SinglePoemPage.tsx
+++ client/src/pages/SinglePoemPage/SinglePoemPage.tsx
@@ -1,32 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./SinglePoemPage.scss";
 
-interface Props {
-  apiUrl: string;
-}
-
-const SinglePoemPage: React.FC<Props> = ({ apiUrl }) => {
+const SinglePoemPage = ({ apiUrl }: { apiUrl: string }) => {
+  // Set poem's state as an empty string
   const [poem, setPoem] = useState(null);
   const { title } = useParams();
-  console.log(title);
-
+  // On render, make a get request to api and use reponse to update state
   useEffect(() => {
     const fetchPoem = async () => {
       try {
-        let response = await axios.get(`${apiUrl}/title/${title}`);
-        setPoem(response.data[0].lines);
+        const response = await axios.get(`${apiUrl}/title/${title}`);
+        setPoem(response.data[0]);
       } catch (error) {
-        console.log(`Error fetching poem: ${error}`);
+        console.error("Error fetching poem: ", error);
       }
     };
     fetchPoem();
-  }, [title]);
+  }, []);
   console.log(poem);
-
+  // Show loading state while waiting for API call to update state of `poem`
+  if (!poem) {
+    return (
+      <div className="poem__loading-container">
+        <h2 className="poem__loading-text">Loading...</h2>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h2>Single Poem</h2>
+    <div className="poem__max-width-container">
+      <h2 className="poem__title appear-1">{poem.title}</h2>
+      <h3 className="poem__author appear-2">{poem.author}</h3>
+      <div className="poem__container--lines">
+        {poem.lines.map((line, index) => (
+          <p className="poem__line appear-3" key={index}>
+            {line}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
