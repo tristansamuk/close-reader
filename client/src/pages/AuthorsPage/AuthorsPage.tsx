b1fc3a12f78ef8cb21230f026bb96337bb1f9bcf
Tristan Samuk <tsamuk@gmail.com>
2024-02-02 13:22:07 -0500

    Create basic list styling

--- client/src/pages/AuthorsPage/AuthorsPage.tsx
+++ client/src/pages/AuthorsPage/AuthorsPage.tsx
@@ -3,7 +3,24 @@ import "./AuthorsPage.scss";
 const AuthorsPage = () => {
   return (
     <div className="authors-page__container--max-width">
-      <h2>Authors</h2>
+      <h2 className="authors-page__heading">Authors</h2>
+      <div className="authors-page__container--list">
+        <div className="authors-page__row">
+          <h3 className="authors-page__name">William Wordsworth</h3>
+        </div>
+        <div className="authors-page__row">
+          <h3 className="authors-page__name">Aphra Behn</h3>
+        </div>
+        <div className="authors-page__row">
+          <h3 className="authors-page__name">Ben Jonson</h3>
+        </div>
+        <div className="authors-page__row">
+          <h3 className="authors-page__name">Anne Carson</h3>
+        </div>
+        <div className="authors-page__row">
+          <h3 className="authors-page__name">William Shakespeare</h3>
+        </div>
+      </div>
     </div>
   );
 };
