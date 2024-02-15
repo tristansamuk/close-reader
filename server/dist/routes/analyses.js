db7fd4d6f674273085554a973ace79308478c485
Tristan Samuk <tsamuk@gmail.com>
2024-02-15 16:24:30 -0500

    Remove conditional check of database because wasn't working

--- server/dist/routes/analyses.js
+++ server/dist/routes/analyses.js
@@ -36,57 +36,51 @@ exports.default = router;
 const openai = new openai_1.default();
 const title = "The Canonization";
 const poet = "John Donne";
+const titleId = 10;
 router.post("/:poemTitle", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // Takes url param and checks database for existing analysis
     try {
-        const poemTitle = req.params.poemTitle;
-        const data = yield (0, db_1.default)("analyses")
-            .join("titles", "titles.id", "analyses.title_id")
-            .select("analyses.id", "analyses.title_id", "titles.title", "analyses.analysis", "titles.short_title")
-            .where("titles.short_title", poemTitle);
-        // If analysis is falsey, makes request to ChatGPT for new analysis
-        // Type error here, undefined?
-        if (!data[0].analysis) {
-            try {
-                // Function that makes the post request to ChatGPT and posts result in the database
-                const sendToGPT = () => __awaiter(void 0, void 0, void 0, function* () {
-                    const completion = yield openai.chat.completions.create({
-                        model: "gpt-3.5-turbo",
-                        messages: [
-                            {
-                                role: "system",
-                                content: "You are poetry expert. When you receive a poem title, please generate a short interpretive analysis that will help a reader understand what the poem is about, any interesting things to pay attention to, and situate the poem in its historical or biographical context.",
-                            },
-                            {
-                                role: "user",
-                                content: `${title} by ${poet}`,
-                            },
-                        ],
-                        temperature: 0.7,
-                        max_tokens: 100,
-                    });
-                    const poemAnalysis = completion.choices[0].message.content;
-                    // Posts the resulting analysis to the database
-                    yield (0, db_1.default)("analyses").insert({
-                        title_id: `${data[0].title_id}`,
-                        analysis: `${poemAnalysis}`,
-                    });
-                    res.status(201).send(poemAnalysis);
-                });
-                // Calling the function
-                sendToGPT();
-            }
-            catch (error) {
-                res.status(500).send("Error getting analysis");
-                console.log(error);
-            }
-        }
-        // What happens if "analysis" is truthy
-        res.json(data);
-        console.log(data[0].analysis);
+        const sendToGPT = () => __awaiter(void 0, void 0, void 0, function* () {
+            const completion = yield openai.chat.completions.create({
+                model: "gpt-3.5-turbo",
+                messages: [
+                    {
+                        role: "system",
+                        content: "You are poetry expert. When you receive a poem title, please generate a short interpretive analysis that will help a reader understand what the poem is about, any interesting things to pay attention to, and situate the poem in its historical or biographical context.",
+                    },
+                    {
+                        role: "user",
+                        content: `${title} by ${poet}`,
+                    },
+                ],
+                temperature: 0.7,
+                max_tokens: 100,
+            });
+            const poemAnalysis = completion.choices[0].message.content;
+            // Posts the resulting analysis to the database
+            yield (0, db_1.default)("analyses").insert({
+                title_id: `${titleId}`,
+                analysis: `${poemAnalysis}`,
+            });
+            res.status(201).send(poemAnalysis);
+        });
+        // Calling the function
+        sendToGPT();
     }
     catch (error) {
-        res.status(500).send("Error checking database for analysis");
+        res.status(500).send("Error getting analysis");
         console.log(error);
     }
 }));
+// Takes url param and checks database for existing analysis
+// const poemTitle = req.params.poemTitle;
+// const data: analysisData[] = await db("analyses")
+//   .join("titles", "titles.id", "analyses.title_id")
+//   .select(
+//     "analyses.id",
+//     "analyses.title_id",
+//     "titles.title",
+//     "analyses.analysis",
+//     "titles.short_title"
+//   )
+//   .where("titles.short_title", poemTitle);
+// If analysis is falsey, makes request to ChatGPT for new analysis
--- server/routes/analyses.ts
+++ server/routes/analyses.ts
@@ -37,72 +37,57 @@ const openai = new OpenAI();
 
 const title = "The Canonization";
 const poet = "John Donne";
+const titleId = 10;
 
 router.post("/:poemTitle", async (req: Request, res: Response) => {
-  // Takes url param and checks database for existing analysis
   try {
-    const poemTitle = req.params.poemTitle;
-    const data: analysisData[] = await db("analyses")
-      .join("titles", "titles.id", "analyses.title_id")
-      .select(
-        "analyses.id",
-        "analyses.title_id",
-        "titles.title",
-        "analyses.analysis",
-        "titles.short_title"
-      )
-      .where("titles.short_title", poemTitle);
-    // If analysis is falsey, makes request to ChatGPT for new analysis
+    const sendToGPT = async () => {
+      const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are poetry expert. When you receive a poem title, please generate a short interpretive analysis that will help a reader understand what the poem is about, any interesting things to pay attention to, and situate the poem in its historical or biographical context.",
+          },
+          {
+            role: "user",
+            content: `${title} by ${poet}`,
+          },
+        ],
+        temperature: 0.7,
+        max_tokens: 100,
+      });
+      const poemAnalysis = completion.choices[0].message.content;
 
-    // Type error here, undefined?
+      // Posts the resulting analysis to the database
 
-    if (!data[0].analysis) {
-      try {
-        // Function that makes the post request to ChatGPT and posts result in the database
+      await db("analyses").insert({
+        title_id: `${titleId}`,
+        analysis: `${poemAnalysis}`,
+      });
+      res.status(201).send(poemAnalysis);
+    };
 
-        const sendToGPT = async () => {
-          const completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [
-              {
-                role: "system",
-                content:
-                  "You are poetry expert. When you receive a poem title, please generate a short interpretive analysis that will help a reader understand what the poem is about, any interesting things to pay attention to, and situate the poem in its historical or biographical context.",
-              },
-              {
-                role: "user",
-                content: `${title} by ${poet}`,
-              },
-            ],
-            temperature: 0.7,
-            max_tokens: 100,
-          });
-          const poemAnalysis = completion.choices[0].message.content;
+    // Calling the function
 
-          // Posts the resulting analysis to the database
-
-          await db("analyses").insert({
-            title_id: `${data[0].title_id}`,
-            analysis: `${poemAnalysis}`,
-          });
-          res.status(201).send(poemAnalysis);
-        };
-
-        // Calling the function
-
-        sendToGPT();
-      } catch (error) {
-        res.status(500).send("Error getting analysis");
-        console.log(error);
-      }
-    }
-
-    // What happens if "analysis" is truthy
-
-    res.json(data);
-    console.log(data[0].analysis);
+    sendToGPT();
   } catch (error) {
-    res.status(500).send("Error checking database for analysis");
+    res.status(500).send("Error getting analysis");
     console.log(error);
   }
 });
+
+// Takes url param and checks database for existing analysis
+// const poemTitle = req.params.poemTitle;
+// const data: analysisData[] = await db("analyses")
+//   .join("titles", "titles.id", "analyses.title_id")
+//   .select(
+//     "analyses.id",
+//     "analyses.title_id",
+//     "titles.title",
+//     "analyses.analysis",
+//     "titles.short_title"
+//   )
+//   .where("titles.short_title", poemTitle);
+// If analysis is falsey, makes request to ChatGPT for new analysis
